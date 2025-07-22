91511529778609c69625b31729f0c46ad3517541
Ricardo Matos <43769547+ricardothadeu@users.noreply.github.com>
2025-07-22 10:36:57 -0300

    Update clock_circuit.v

--- clock_circuit.v
+++ clock_circuit.v
@@ -1,38 +1,22 @@
-//module clock_circuit (
-//    
-//	 input prog_run, 
-//    input manual_auto,        // quando 1 = automático (clock do FPGA), 0 = manual (botão)
-//    input clock_auto,         // clock do proprio FPGA
-//    input clock_manual,       // pulso único vindo do botão
-//    input hlt_sig,            // sinal de controle que interrompe o processamento quando for 1
-//    output clock_sap          // clock final que alimenta o SAP-1
-//);
-//
-//    assign clock_sap = (prog_run && ((manual_auto && clock_auto) || (!manual_auto && clock_manual)) && !hlt_sig);
-//
-//endmodule
-
 module clock_circuit (
-    input  clock_fpga,
-    input  prog_run,
-    input  selecao_manual_auto,
-    input  key0,
-    input  hlt_sig,
-    output clock_sap
+    input  wire clock_fpga,
+    input  wire prog_run,
+    input  wire selecao_manual_auto,
+    input  wire key0,       // botão ativo‑baixa
+    input  wire hlt_sig,
+    output wire clock_sap
 );
 
-    reg  key0_d;
-    wire key0_rise = (key0_d == 1'b0) && (key0 == 1'b1);
-
-    // Sincroniza o botão
-    always @(posedge clock_fpga) begin
+    // armazena valor anterior
+    reg key0_d;
+    always @(posedge clock_fpga)
         key0_d <= key0;
-    end
 
-    // Gera clock_sap combinacionalmente
-    assign clock_sap = //(!prog_run || hlt_sig) ? 1'b0 : 
-							  (hlt_sig) ? 1'b0 :
-                       (selecao_manual_auto) ? clock_fpga :
-                       key0_rise;
+    // detecta pressão do botão (1→0)
+    wire key0_fall = key0_d && ~key0;
+
+    assign clock_sap =
+        (hlt_sig)              ? 1'b0 :    // se halt, sem clock
+        (selecao_manual_auto)  ? clock_fpga: // modo auto
+                                 key0_fall; // manual: pulso ao apertar
 endmodule
-
